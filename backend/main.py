bd0ad25f402f79e8051bbb81d651205ed6789f3d
nathanAndrews03 <andre149@purdue.edu>
2025-06-27 11:45:13 -0400

    api setup

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
 # backend/main.py
 from fastapi import FastAPI, Query
 from typing import List
+import os
 import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
-
-with open("recipes.json") as f:
+with open("recipes/recipes.json") as f:
     RECIPES = json.load(f)
+
+SPOON_KEY = os.getenv("SPOON_KEY")
 
 @app.get("/recipes")
 def get_recipes(region: str = None):
@@ -20,3 +26,9 @@ def get_by_ingredients(ingredients: List[str]):
         r for r in RECIPES
         if all(i in r["ingredients"] for i in ingredients)
     ]
+
+@app.get("/recipes/external")
+def get_random_recipes():
+    url = f"https://api.spoonacular.com/recipes/random?number=10&apiKey={SPOON_KEY}"
+    response = requests.get(url)
+    return response.json()
